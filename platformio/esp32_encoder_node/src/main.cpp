b4f17bdc821d32037e9664afb7c1d335e0d9ce2d
Mayman836 <mohamedaymanforbusiness@gmail.com>
2026-01-30 20:15:59 +0200

    day 9.2: used PCNT instead of state-table because its cleaner and more reliable, whats left is making the encoder ROS node itself for the 3 encoders

--- platformio/esp32_encoder_node/src/main.cpp
+++ platformio/esp32_encoder_node/src/main.cpp
@@ -1,33 +1,145 @@
+// PCNT
+
 #include <Arduino.h>
+#include <driver/pcnt.h>
 
 #define ENC_A 25
 #define ENC_B 26
 
-volatile long encoderCount = 0;
+#define PCNT_UNIT PCNT_UNIT_0
 
-void IRAM_ATTR isrA() {
-  if (!digitalRead(ENC_B))
-    encoderCount++;
-  else
-    encoderCount--;
-}
+#define PUB_INTERVAL_MS 10
+
+volatile int32_t encoderTotal = 0;
+unsigned long lastPub = 0;
 
 void setup() {
   Serial.begin(115200);
 
-  pinMode(ENC_A, INPUT);
-  pinMode(ENC_B, INPUT);
+  pcnt_config_t pcnt_config = {};
 
-  attachInterrupt(digitalPinToInterrupt(ENC_A), isrA, RISING);
+  pcnt_config.pulse_gpio_num = ENC_A;
+  pcnt_config.ctrl_gpio_num = ENC_B;
+  pcnt_config.channel = PCNT_CHANNEL_0;
+  pcnt_config.unit = PCNT_UNIT;
+
+  pcnt_config.pos_mode = PCNT_COUNT_INC;
+  pcnt_config.neg_mode = PCNT_COUNT_DEC;
+  pcnt_config.lctrl_mode = PCNT_MODE_REVERSE;
+  pcnt_config.hctrl_mode = PCNT_MODE_KEEP;
+
+  pcnt_config.counter_h_lim = 32767;
+  pcnt_config.counter_l_lim = -32767;
+
+  pcnt_unit_config(&pcnt_config);
+
+  pcnt_set_filter_value(PCNT_UNIT, 100);
+  pcnt_filter_enable(PCNT_UNIT);
+
+  pcnt_counter_pause(PCNT_UNIT);
+  pcnt_counter_clear(PCNT_UNIT);
+  pcnt_counter_resume(PCNT_UNIT);
 }
 
 void loop() {
-  static long last = 0;
+  unsigned long now = millis();
 
-  if (last != encoderCount) {
-    Serial.println(encoderCount);
-    last = encoderCount;
+  if ((now - lastPub) >= PUB_INTERVAL_MS) {
+    int16_t delta = 0;
+
+    pcnt_get_counter_value(PCNT_UNIT, &delta);
+    pcnt_counter_clear(PCNT_UNIT);
+
+    encoderTotal += delta;
+
+    Serial.println(encoderTotal);
+    lastPub = now;
   }
-
-  delay(10);
 }
+
+// 4x Decoding (2400 pps)
+
+// #include <Arduino.h>
+
+// #define ENC_A 25
+// #define ENC_B 26
+
+// volatile long encoderCount = 0;
+// volatile uint8_t prevState = 0;
+
+// const unsigned long PUB_INTERVAL_MS = 10;
+// unsigned long lastPub = 0;
+
+// const int8_t quadTable[16] = {
+//   0, -1,  1,  0,
+//   1,  0,  0, -1,
+//  -1,  0,  0,  1,
+//   0,  1, -1,  0
+// };
+
+// void IRAM_ATTR encoderISR() {
+//   uint8_t state = (digitalRead(ENC_A) << 1) | digitalRead(ENC_B);
+//   uint8_t index = (prevState << 2) | state;
+//   encoderCount += quadTable[index];
+//   prevState = state;
+// }
+
+// void setup() {
+//   Serial.begin(115200);
+
+//   pinMode(ENC_A, INPUT_PULLUP);
+//   pinMode(ENC_B, INPUT_PULLUP);
+
+//   prevState = (digitalRead(ENC_A) << 1) | digitalRead(ENC_B);
+
+//   attachInterrupt(digitalPinToInterrupt(ENC_A), encoderISR, CHANGE);
+//   attachInterrupt(digitalPinToInterrupt(ENC_B), encoderISR, CHANGE);
+// }
+
+// void loop() {
+//   unsigned long now = millis();
+
+//   if ((now - lastPub) >= PUB_INTERVAL_MS) {
+//     noInterrupts();
+//     long count = encoderCount;
+//     interrupts();
+//     Serial.println(count);
+//     lastPub = now;
+//   }
+// }
+
+// 1x Decoding (600 pps)
+
+// #include <Arduino.h>
+
+// #define ENC_A 25
+// #define ENC_B 26
+
+// volatile long encoderCount = 0;
+
+// void IRAM_ATTR isrA() {
+//   if (!digitalRead(ENC_B))
+//     encoderCount++;
+//   else
+//     encoderCount--;
+// }
+
+// void setup() {
+//   Serial.begin(115200);
+
+//   pinMode(ENC_A, INPUT_PULLUP);
+//   pinMode(ENC_B, INPUT_PULLUP);
+
+//   attachInterrupt(digitalPinToInterrupt(ENC_A), isrA, RISING);
+// }
+
+// void loop() {
+//   static long last = 0;
+
+//   if (last != encoderCount) {
+//     Serial.println(encoderCount);
+//     last = encoderCount;
+//   }
+
+//   delay(10);
+// }
